eae9201a8f62fa5c949ed3091805876d3cfa1ff1
Gustav Pursche <pursche@posteo.de>
2020-12-27 15:56:40 +0100

    ChapterList: improve styles on small screens

--- web/components/ChapterList/chapterList.styles.js
+++ web/components/ChapterList/chapterList.styles.js
@@ -3,7 +3,12 @@ import { css } from '@emotion/react';
 import boxShadow from '../../lib/styles/box-shadow';
 
 export const accordion = css`
-  margin: 8rem -4rem 0 -4rem;
+  margin: 8rem -1.5rem 0 -1.5rem;
+
+  @media (min-width: 768px) {
+    margin-left: -4rem;
+    margin-right: -4rem;
+  }
 `;
 
 export const accordionButton = css`
@@ -14,8 +19,13 @@ export const accordionButton = css`
   border-radius: 1rem 0 0 0;
   cursor: pointer;
   display: flex;
-  font-size: 3rem;
-  padding: 1rem 1rem 2rem 4rem;
+  font-size: 2.1rem;
+  padding: 1rem 1.5rem 1.5rem 1.5rem;
+
+  @media (min-width: 768px) {
+    font-size: 3rem;
+    padding-bottom: 2rem;
+  }
 
   :hover,
   :focus {
@@ -24,7 +34,15 @@ export const accordionButton = css`
 `;
 
 export const accordionButtonIcon = css`
-  margin-right: 1rem;
+  @media (max-width: 768px) {
+    justify-self: flex-end;
+    margin-left: auto;
+    order: 2;
+  }
+
+  @media (min-width: 768px) {
+    margin-right: 1rem;
+  }
 `;
 
 export const accordionItem = css`
@@ -33,5 +51,9 @@ export const accordionItem = css`
 
 export const accordionPanel = css`
   max-width: 67rem;
-  padding: 2rem 0 2rem 4rem;
+  padding: 1rem 1.5rem 1.5rem 1.5rem;
+
+  @media (min-width: 768px) {
+    padding: 2rem 0 2rem 4rem;
+  }
 `;
