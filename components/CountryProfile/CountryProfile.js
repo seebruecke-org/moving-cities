77ffb34e8b36dee7933ccb959756fedfbb2311bc
Gustav Pursche <pursche@posteo.de>
2021-02-11 22:42:31 +0100

    CountryProfile: render content blocks

--- components/CountryProfile/CountryProfile.js
+++ components/CountryProfile/CountryProfile.js
@@ -1,3 +1,15 @@
-export default function CountryProfile() {
-  return null;
+import Blocks from '../Blocks';
+
+import * as styles from './countryProfile.styles';
+
+export default function CountryProfile({ name, content }) {
+  return (
+    <div css={styles.container}>
+      <h1 css={styles.title}>{name}</h1>
+
+      <div css={styles.blocksContainer}>
+        <Blocks blocks={content} />
+      </div>
+    </div>
+  );
 }
