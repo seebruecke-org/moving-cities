eaf0a191306da862f6ffa79fcdb0b91f5a9c17e0
Gustav Pursche <pursche@posteo.de>
2021-02-23 20:25:47 +0100

    MapCityMarker: make everything smaller on small screens

--- components/MapCityMarker/mapCityMarker.styles.js
+++ components/MapCityMarker/mapCityMarker.styles.js
@@ -12,20 +12,34 @@ export const container = css`
 
 export const icon = css`
   ${ICON_BASE}
-  height: 0.75rem;
-  width: 0.75rem;
+  height: 0.5rem;
+  width: 0.5rem;
+
+  @media (min-width: 768px) {
+    height: 0.75rem;
+    width: 0.75rem;
+  }
 `;
 
 export const profileIcon = css`
   ${ICON_BASE}
-  height: 2.75rem;
-  width: 2.75rem;
+  height: 2rem;
+  width: 2rem;
+
+  @media (min-width: 768px) {
+    height: 2.75rem;
+    width: 2.75rem;
+  }
 `;
 
 export const name = css`
   font-family: var(--font-sans);
-  font-size: 1.1rem;
+  font-size: 0.95rem;
   line-height: 1;
   max-width: 9rem;
   text-align: center;
+
+  @media (min-width: 768px) {
+    font-size: 1.1rem;
+  }
 `;
