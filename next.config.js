495def6005e8bf5402624dbe17a20b418ae2024b
Gustav Pursche <pursche@posteo.de>
2021-07-14 14:11:10 +0200

    Next: remove next-fonts

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
-const withFonts = require('next-fonts');
 const withPlugins = require('next-compose-plugins');
 
-module.exports = withPlugins([withFonts], {
+module.exports = withPlugins([], {
   i18n: {
     locales: ['en'],
     defaultLocale: 'en'
