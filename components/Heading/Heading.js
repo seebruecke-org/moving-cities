c82cea868fd1f473510e5be60cd781db46cec4cf
Gustav Pursche <pursche@posteo.de>
2021-07-28 14:04:06 +0200

    Heading: fix h2 font-size on large screens

--- components/Heading/Heading.js
+++ components/Heading/Heading.js
@@ -4,7 +4,7 @@ export default function Heading({ level = 1, as, children, className, ...props }
   const Tag = `h${as || level}`;
   const styles = {
     h1: 'text-3xl md:text-6xl leading-tight font-bold font-raptor',
-    h2: 'text-2xl md:text-l md:text-4xl leading-tight font-bold font-raptor',
+    h2: 'text-2xl md:text-4xl leading-tight font-bold font-raptor',
     h3: 'text-3xl leading-tight font-bold font-raptor',
     h4: 'text-2xl leading-tight font-bold font-raptor'
   };
