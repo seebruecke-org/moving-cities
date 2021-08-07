b041b4d6d4c7d255317833fe8a0232c1dafe9a28
Gustav Pursche <pursche@posteo.de>
2021-08-07 17:02:12 +0200

    Select: improve default styles

--- components/Select/Select.js
+++ components/Select/Select.js
@@ -1,3 +1,109 @@
-import ReactSelect from 'react-select';
+import ReactSelect, { components as C } from 'react-select';
 
-export default ReactSelect;
+const styles = {
+  control: (provided, state) => ({
+    ...provided,
+    backgroundColor: '#FAF11A',
+    border: state.isFocused ? '1px solid black' : '1px solid #FAF11A',
+    boxShadow: '0 0.4rem 1.6rem rgba(0, 0, 0, .25)',
+    color: 'black',
+    fontFamily: 'Raptor',
+    padding: '0.4rem 0.8rem',
+    zIndex: 20,
+
+    '&:hover': {
+      border: '1px solid black'
+    }
+  }),
+
+  indicatorSeparator: () => ({
+    display: 'none'
+  }),
+
+  menu: (provided) => ({
+    ...provided,
+    boxShadow: '0 0.4rem 1.6rem rgba(0, 0, 0, .25)',
+    marginTop: 0,
+    zIndex: 10
+  }),
+
+  menuList: (provided) => ({
+    ...provided,
+    padding: 0
+  }),
+
+  option: (provided, state) => {
+    const { options } = state;
+    const index = options.findIndex(option => option.label === state.label);
+
+    return {
+      ...provided,
+      backgroundColor: state.isSelected ? '#FAF11A' : 'transparent',
+      border: '0',
+      color: 'black',
+      fontFamily: 'Raptor',
+      fontSize: '1.6rem',
+      padding: '1.2rem 1.8rem',
+      position: 'relative',
+
+      '&:hover': {
+        backgroundColor: '#FAF11A',
+        cursor: 'pointer'
+      },
+
+      '&:before': index !== 0 ? {
+        backgroundColor: '#E1E1E1',
+        content: '""',
+        height: '1px',
+        left: '1rem',
+        position: 'absolute',
+        right: '1rem',
+        top: '0',
+      } : {}
+    }
+  },
+
+  placeholder: (provided) => ({
+    ...provided,
+    color: 'black',
+    fontFamily: 'Raptor',
+    fontSize: '1.6rem',
+  }),
+
+  singleValue: (provided) => ({
+    ...provided,
+    color: 'black',
+    fontFamily: 'Raptor',
+    fontSize: '1.6rem',
+  }),
+
+  valueContainer: (provided) => ({
+    ...provided,
+    color: 'black'
+  })
+};
+
+function DropdownIndicator(props) {
+  if (props.isFocused === true) {
+    return <C.DropdownIndicator {...props}>
+    <svg xmlns="http://www.w3.org/2000/svg" width="28" height="17" viewBox="0 0 28 17" fill="none">
+      <path stroke="#F55511" stroke-linecap="round" stroke-width="3" d="M26 15L14 3 2 15"/>
+    </svg>
+  </C.DropdownIndicator>;
+  }
+
+  return <C.DropdownIndicator {...props}>
+    <svg xmlns="http://www.w3.org/2000/svg" width="28" height="17" viewBox="0 0 28 17" fill="none">
+      <path stroke="#F55511" stroke-linecap="round" stroke-width="3" d="M2 2l12 12L26 2"/>
+    </svg>
+  </C.DropdownIndicator>;
+}
+
+export default function Select(props) {
+  return <ReactSelect
+    styles={styles}
+    isSearchable={false}
+    /*defaultMenuIsOpen*/
+    components={{ DropdownIndicator }}
+    {...props} />
+};
