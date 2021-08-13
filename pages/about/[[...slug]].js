8203d145c0b787ad5b729dfbe8dcf9baa61e0997
Gustav Pursche <pursche@posteo.de>
2021-08-13 17:29:21 +0200

    About: load content dynamically

---
+++ pages/about/[[...slug]].js
@@ -0,0 +1,62 @@
+import BlockSwitch from '@/components/Blocks/BlockSwitch';
+import Heading from '@/components/Heading';
+import SEO from '@/components/SEO';
+import SidebarMenu from '@/components/SidebarMenu';
+
+import { fetchAboutBySlug, fetchAllAboutPaths, fetchAllAbouts } from '@/lib/abouts';
+import { getTranslations } from '@/lib/global';
+
+export default function About({ navigation, about: { title, content }}) {
+  return (
+    <div className="md:flex">
+      <SEO title="About" />
+
+      <SidebarMenu items={navigation.map(({ title, slug }) => ({
+        target: `/about/${slug}`,
+        label: title
+      }))} />
+
+      <article>
+        <Heading level={1} className="pl-8 md:pl-10 pt-10">
+          {title}
+        </Heading>
+
+        <BlockSwitch blocks={content} />
+      </article>
+    </div>
+  );
+}
+
+export async function getStaticPaths({ locales }) {
+  const abouts = await Promise.all(locales.map(async locale => await fetchAllAboutPaths(locale)));
+
+  const paths = abouts.flat().map(({ slug }) => ({
+    params: { slug: [slug] }
+  }));
+
+  return {
+    paths,
+    fallback: 'blocking'
+  };
+}
+
+export async function getStaticProps({ locale, params: { slug } }) {
+  const translations = await getTranslations(locale, ['city']);
+  const about = await fetchAboutBySlug(slug, locale);
+  const navigation = await fetchAllAbouts(locale);
+
+  if (about === null) {
+    return {
+      notFound: true
+    }
+  }
+
+  return {
+    revalidate: 60,
+    props: {
+      ...translations,
+      navigation,
+      about
+    }
+  };
+}
