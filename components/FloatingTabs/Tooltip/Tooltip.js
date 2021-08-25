cfadb937da25998ef258c4df885c26c53fd93eca
Gustav Pursche <pursche@posteo.de>
2021-08-25 23:36:21 +0200

    FloatingTabs: hide tooltip arrow on small screens

--- components/FloatingTabs/Tooltip/Tooltip.js
+++ components/FloatingTabs/Tooltip/Tooltip.js
@@ -3,7 +3,7 @@ import Paragraph from '@/components/Paragraph';
 export default function Tooltip({ children }) {
   return (
     <div className="md:absolute md:top-full md:left-2/4 md:bg-white p-8 md:rounded-lg md:border-2 w-full md:-translate-x-2/4 md:mt-4">
-      <span className="block w-6 h-6 bg-white border-l-2 border-t-2 rotate-45 absolute top-0 -translate-y-3 z-20 left-20" />
+      <span className="hidden md:block w-6 h-6 bg-white border-l-2 border-t-2 rotate-45 absolute top-0 -translate-y-3 z-20 left-20" />
 
       <Paragraph isSmall>{children}</Paragraph>
     </div>
