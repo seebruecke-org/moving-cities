6e85339ebcdbcff1b942e08b2dd84353401e4c1e
Gustav Pursche <pursche@posteo.de>
2021-08-25 23:29:25 +0200

    Approach: fix read more alignment

--- components/Approach/Approach.js
+++ components/Approach/Approach.js
@@ -29,7 +29,7 @@ export default function Approach({ title, city, categories = [], uri }) {
           ))}
         </ul>
 
-        <button type="button" tabIndex="-1" className="font-raptor font-bold text-xl mt-auto justify-end">
+        <button type="button" tabIndex="-1" className="font-raptor font-bold text-xl mt-auto justify-end self-start">
           {t('readMore')}
           <span className="group-hover:text-red-300 ml-2">⟶</span>
         </button>
