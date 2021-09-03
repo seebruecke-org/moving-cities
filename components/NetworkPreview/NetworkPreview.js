af621677db54b415314a7fc58bfe9e10fd178bcd
Gustav Pursche <pursche@posteo.de>
2021-09-03 19:29:04 +0200

    NetworkPreview: improve bottom spacing

--- components/NetworkPreview/NetworkPreview.js
+++ components/NetworkPreview/NetworkPreview.js
@@ -14,7 +14,7 @@ export default function NetworkPreview({ title, content, featuredCities, cities 
   return (
     <article
       className={clsx(
-        'bg-white h-full p-8 md:p-10 overflow-y-auto pb-28 md:pb-0',
+        'bg-white h-full p-8 md:p-10 overflow-y-auto pb-28',
         styles.networkPreview
       )}>
       <Heading level={1} as={2}>
