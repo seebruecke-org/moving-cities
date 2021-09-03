9106007a2fb893fd0a43eb28cf85b375d36df85a
Gustav Pursche <pursche@posteo.de>
2021-09-03 19:17:16 +0200

    Quote: improve font-size on small screens

--- components/Blocks/Quote/Quote.js
+++ components/Blocks/Quote/Quote.js
@@ -1,7 +1,7 @@
 export default function Quote({ quoteAuthor: author, quoteContent: content }) {
   return (
     <blockquote className="w-full grid grid-cols-10 max-w-8xl my-8 md:my-16 pr-10 pl-10 md:pr-10">
-      <p className="text-4xl md:text-3xl font-bold font-raptor leading-tight col-span-full md:col-span-8">
+      <p className="text-2xl md:text-3xl font-bold font-raptor leading-tight col-span-full md:col-span-8">
         <span className="text-red-300">“</span>
         {content}
         <span className="text-red-300">”</span>
