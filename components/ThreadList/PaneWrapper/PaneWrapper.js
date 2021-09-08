7260a8485f98eb7bc8c4c92ca40e820e5e885104
Gustav Pursche <pursche@posteo.de>
2021-09-08 13:24:26 +0200

    PaneWrapper: hide shadows on small screens

--- components/ThreadList/PaneWrapper/PaneWrapper.js
+++ components/ThreadList/PaneWrapper/PaneWrapper.js
@@ -1,9 +1,9 @@
 export default function PaneWrapper({ children }) {
   return (
     <div className="md:absolute md:top-0 md:left-full md:h-full z-10">
-      <span className="absolute top-0 left-0 w-3 bg-gradient-to-r from-black to-transparent h-full opacity-20" />
+      <span className="absolute top-0 left-0 w-3 bg-gradient-to-r from-black to-transparent h-full opacity-20 hidden md:block" />
       {children}
-      <span className="absolute top-0 left-full w-3 bg-gradient-to-r from-black to-transparent h-full opacity-20" />
+      <span className="absolute top-0 left-full w-3 bg-gradient-to-r from-black to-transparent h-full opacity-20 hidden md:block" />
     </div>
   );
 }
