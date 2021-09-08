276863bd25845d0646cdeb19eaa3ed3b736579d1
Gustav Pursche <pursche@posteo.de>
2021-09-08 09:57:13 +0200

    Map: fix screen overflows

--- pages/_app.js
+++ pages/_app.js
@@ -30,7 +30,7 @@ function CustomApp({ Component, pageProps: { state, ...pageProps } }) {
     <Provider createStore={createStore}>
       <Menu />
 
-      <main className="relative md:ml-24 md:h-screen">
+      <main className="relative md:pl-24 md:h-screen md:overflow-hidden">
         <Component {...pageProps} routeHasChanged={routeHasChanged} />
       </main>
     </Provider>
