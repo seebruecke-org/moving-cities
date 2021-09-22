c7c8c34754ff746bffbbfdb486193dd343d83545
Gustav Pursche <pursche@posteo.de>
2021-09-22 11:59:47 +0200

    ThreadList: prevent list from shrinking

--- components/ThreadList/ThreadList.js
+++ components/ThreadList/ThreadList.js
@@ -48,7 +48,7 @@ export default function ThreadList({
         <span className="absolute top-0 left-full w-3 bg-gradient-to-r from-black h-full opacity-20 hidden md:block" />
       )}
 
-      <ul className="md:h-full md:overflow-y-auto md:w-72">
+      <ul className="md:overflow-y-auto md:w-72 flex-shrink-0">
         {items.map(({ className, __typename, ...item }, index) => {
           const isActive = paneData?.index === index;
 
