a4f94ceab6769e69c33eff9acefbc4d6ae0cbcde
Gustav Pursche <pursche@posteo.de>
2021-09-22 14:19:13 +0200

    Media: render alternative texts and captions

--- components/Blocks/Media/Media.js
+++ components/Blocks/Media/Media.js
@@ -4,19 +4,28 @@ import Columns from '@/components/Columns';
 
 import { buildCMSUrl } from '@/lib/api';
 
-export default function Media({ media: { url, width, height } }) {
+export default function Media({ media: { url, width, height, alternativeText, caption } }) {
   const props = {
     layout: 'responsive',
     src: buildCMSUrl(url),
     width,
-    height
+    height,
+    alt: alternativeText
   };
 
   return (
     <Columns className="my-6 md:my-20 max-w-8xl px-8 md:pl-10">
       <span />
 
-      <NextImage {...props} />
+      <figure>
+        <NextImage {...props} />
+
+        {caption && (
+          <figcaption className="font-raptor text-s leading-none mt-4">
+            {caption}
+          </figcaption>
+        )}
+      </figure>
     </Columns>
   );
 }
--- components/Blocks/Media/block.js
+++ components/Blocks/Media/block.js
@@ -3,5 +3,7 @@ export default `
     url
     width
     height
+    alternativeText
+    caption
   }
 `;
