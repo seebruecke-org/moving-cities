cc7eae6688fb98990151063e9af87a4d54961e04
Gustav Pursche <pursche@posteo.de>
2021-09-24 22:18:21 +0200

    Tooltip: improve position

--- components/FloatingTabs/Tooltip/Tooltip.js
+++ components/FloatingTabs/Tooltip/Tooltip.js
@@ -6,7 +6,7 @@ export default function Tooltip({ children, style, ...props }) {
       <span
         className="hidden md:block w-6 h-6 bg-white border-l-2 border-t-2 absolute top-0 z-20 left-20"
         style={{
-          transform: 'rotate(45deg) translateY(calc(-50% - 5px))',
+          transform: 'rotate(45deg) translateX(-50%) translateY(calc(-50% + 2px))',
           ...style
         }}
         {...props}
