f59d695fa7fea2ebf38979eae391ab9e04290d74
Gustav Pursche <pursche@posteo.de>
2021-09-29 10:10:14 +0200

    Heading: fix h3 font-size on larger screens

--- components/Heading/Heading.js
+++ components/Heading/Heading.js
@@ -5,7 +5,7 @@ export default function Heading({ level = 1, as, children, className, ...props }
   const styles = {
     h1: 'text-4xl md:text-5xl leading-tight font-bold font-raptor',
     h2: 'text-2xl md:text-3xl leading-tight font-bold font-raptor',
-    h3: 'text-xl leading-tight font-bold font-raptor',
+    h3: 'text-xl md:text-2xl leading-tight font-bold font-raptor',
     h4: 'text-l leading-tight font-bold font-raptor',
     h5: 'text-xl md:text-m leading-tight font-semibold md:font-bold font-raptor'
   };
