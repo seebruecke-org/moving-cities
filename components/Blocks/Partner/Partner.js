872932e5ec299b5281e41bbe359428a75d9d573c
Gustav Pursche <pursche@posteo.de>
2021-09-29 18:02:57 +0200

    Partner: move logo below text

--- components/Blocks/Partner/Partner.js
+++ components/Blocks/Partner/Partner.js
@@ -7,19 +7,20 @@ import { buildCMSUrl } from '@/lib/api';
 export default function Partner({ partnerLogo, partnerContent }) {
   return (
     <Columns className="md:my-8 max-w-8xl pl-8 pr-8 md:pr-0 md:pl-10">
-      <div className="flex w-full">
-        <div className="max-w-1/2 w-full md:ml-auto mb-10 md:mb-0">
-          <Image
+      <div />
+
+      <div>
+        <Markdown isSmall={false}>{partnerContent}</Markdown>
+
+        <Image
             src={buildCMSUrl(partnerLogo.url)}
             layout="responsive"
             width={partnerLogo.width}
             height={partnerLogo.height}
             alt={partnerLogo.alternativeText}
+            className="max-w-1/2"
           />
-        </div>
       </div>
-
-      <Markdown isSmall={false}>{partnerContent}</Markdown>
     </Columns>
   );
 }
