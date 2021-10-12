b7106d2b4ec47adb6af8de0b577ceb49c383723a
Gustav Pursche <pursche@posteo.de>
2021-10-12 20:48:28 +0200

    i18n: create localized rewrites

--- next.config.js
+++ next.config.js
@@ -3,11 +3,51 @@ const withPreact = require('next-plugin-preact');
 
 const { i18n } = require('./next-i18next.config');
 
+function createLocalizedRewrites() {
+  const { locales, defaultLocale } = i18n;
+  const base = require(`./locales/${defaultLocale}/slugs.json`);
+
+  return locales.filter(locale => defaultLocale !== locale).reduce((acc, locale) => {
+    const slugs = require(`./locales/${locale}/slugs.json`);
+    const keys = Object.keys(slugs);
+    const rewrites = keys.map((key) => {
+      const source = `/${locale}/${slugs[key]}`;
+      const destination = `/${locale}/${base[key]}`;
+
+      if (source === destination) {
+        return null;
+      }
+
+      console.log('Rewrite', source, ' > ', destination);
+
+      return {
+        source,
+        destination,
+        locale: false
+      }
+    }).filter(Boolean);
+
+    return [
+      ...acc,
+      ...rewrites
+    ];
+  }, []);
+}
+
 module.exports = withPlugins([withPreact], {
   i18n,
 
   images: {
-    domains: [process.env.NEXT_IMAGE_HOSTNAME]
+    domains: [process.env.NEXT_IMAGE_HOSTNAME],
+    minimumCacheTTL: 60 * 60 * 24
+  },
+
+  async rewrites() {
+    const rewrites = createLocalizedRewrites();
+
+    return {
+      beforeFiles: rewrites
+    };
   },
 
   experimental: {
