3c741e93ec6fb7865e7c2e0026a865cfffc44f37
Gustav Pursche <pursche@posteo.de>
2021-10-13 15:28:33 +0200

    Partner: render title if one is set

--- components/Blocks/Partner/Partner.js
+++ components/Blocks/Partner/Partner.js
@@ -1,4 +1,5 @@
 import Columns from '@/components/Columns';
+import Heading from '@/components/Heading';
 import Image from 'next/image';
 import Markdown from '@/components/Markdown';
 
@@ -7,7 +8,13 @@ import { buildCMSUrl } from '@/lib/api';
 export default function Partner({ partnerLogo, partnerContent }) {
   return (
     <Columns className="md:my-8 max-w-8xl pl-8 pr-8 md:pr-0 md:pl-10 pb-20">
-      <div />
+      <div>
+        {partnerTitle && (
+          <Heading level={2}>
+            {partnerTitle}
+          </Heading>
+        )}
+      </div>
 
       <div>
         <Markdown isSmall={false}>{partnerContent}</Markdown>
--- components/Blocks/Partner/block.js
+++ components/Blocks/Partner/block.js
@@ -6,5 +6,6 @@ export default `
     height
   }
 
+  partnerTitle: title
   partnerContent: content
 `;
