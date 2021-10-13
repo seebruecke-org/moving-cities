7073e8e59a7eac67d944eeda26797ea5ab1990c5
Gustav Pursche <pursche@posteo.de>
2021-10-13 11:15:45 +0200

    List: improve decimal styling

--- components/List/List.js
+++ components/List/List.js
@@ -3,5 +3,5 @@ import clsx from "clsx";
 export default function List({ ordered, children }) {
   const TagName = ordered ? 'ol' : 'ul';
 
-  return <TagName className={clsx("ml-4 mt-4", ordered ? 'list-decimal' : 'list-disc')}>{children}</TagName>;
+  return <TagName className={clsx("ml-8 mt-4", ordered ? 'list-decimal' : 'list-disc')}>{children}</TagName>;
 }
