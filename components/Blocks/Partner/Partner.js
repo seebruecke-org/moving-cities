fe26a6718fe39a63c2b09d958ee97426db0f242d
Gustav Pursche <pursche@posteo.de>
2021-10-13 10:36:24 +0200

    Partner: reduce size of the logo

--- components/Blocks/Partner/Partner.js
+++ components/Blocks/Partner/Partner.js
@@ -6,20 +6,21 @@ import { buildCMSUrl } from '@/lib/api';
 
 export default function Partner({ partnerLogo, partnerContent }) {
   return (
-    <Columns className="md:my-8 max-w-8xl pl-8 pr-8 md:pr-0 md:pl-10">
+    <Columns className="md:my-8 max-w-8xl pl-8 pr-8 md:pr-0 md:pl-10 pb-20">
       <div />
 
       <div>
         <Markdown isSmall={false}>{partnerContent}</Markdown>
 
-        <Image
-          src={buildCMSUrl(partnerLogo.url)}
-          layout="responsive"
-          width={partnerLogo.width}
-          height={partnerLogo.height}
-          alt={partnerLogo.alternativeText}
-          className="max-w-1/2"
-        />
+        <div className="max-w-xs mt-16">
+          <Image
+            src={buildCMSUrl(partnerLogo.url)}
+            layout="responsive"
+            width={partnerLogo.width}
+            height={partnerLogo.height}
+            alt={partnerLogo.alternativeText}
+          />
+        </div>
       </div>
     </Columns>
   );
