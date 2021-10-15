7fa156533e81f111529aacbf812c160f839a3410
Gustav Pursche <pursche@posteo.de>
2021-10-15 14:07:45 +0200

    Intro: improve font-size and line-height

--- components/Blocks/Intro/Intro.js
+++ components/Blocks/Intro/Intro.js
@@ -6,7 +6,7 @@ export default function Section({ introContent: content }) {
     <Columns className="max-w-8xl">
       <span />
 
-      <Paragraph className="text-xl md:text-2xl font-bold px-8 md:pr-0 leading-tight">
+      <Paragraph isSmall={true} className="text-xl md:text-2xl font-bold px-8 md:pr-0 leading-[1.2] lg:leading-tight">
         {content}
       </Paragraph>
     </Columns>
