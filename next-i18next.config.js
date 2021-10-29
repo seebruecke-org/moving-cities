2e5de97ab24be3b50dd4310a8d372bd5d8703a67
Gustav Pursche <pursche@posteo.de>
2021-10-29 11:55:07 +0200

    i18n: enable it and pl locales

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   i18n: {
     defaultLocale: 'en',
-    locales: ['en', 'de', /*'it',*/ 'gr' /*'pl', 'fr', 'es'*/]
+    locales: ['en', 'de', 'it', 'gr', 'pl'/*, 'fr', 'es'*/]
   },
 
   localePath: path.resolve('./locales')
