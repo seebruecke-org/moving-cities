02c4c98a72857a80b7f94d6f301a06b56341c1a1
Gustav Pursche <pursche@posteo.de>
2021-11-26 14:37:10 +0100

    Matomo: track action name

--- pages/api/track.js
+++ pages/api/track.js
@@ -12,7 +12,10 @@ export default function (req, res) {
 
   const { url } = req.query;
 
-  tracker.track(url);
+  tracker.track({
+    action_name: url,
+    url: `https://moving-cities.eu${url}`
+  });
 
   res.send({});
 }
