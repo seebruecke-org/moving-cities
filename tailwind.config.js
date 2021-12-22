92ded1bfb415a3638358a613ce765da41f6910ec
Gustav Hansen <gu@stav.dev>
2021-12-22 15:33:11 +0100

    chore: update dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  mode: 'jit',
-  darkMode: false,
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false,
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}'
+  ],
   theme: {
     colors: {
       yellow: {
