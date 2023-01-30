5d0625ce4acdef796a53a01d8f4659b0087c6039
volkram <volkram@volkramweber.de>
2023-01-30 19:52:59 +0100

    load menu on landing-page

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,8 @@ const Intro = dynamic(() => import('@/components/Intro'));
 import { createClient } from '@/lib/api';
 import { fetchCounts } from '@/lib/cities';
 import { fetchIntro } from '@/lib/intro';
-import {getTranslations} from "@/lib/global";
+import { getTranslations } from "@/lib/global";
+import { fetchMenu } from '@/lib/menu';
 
 
 export default function HomePage({ intro, counts }) {
@@ -21,13 +22,15 @@ export async function getStaticProps({ locale }) {
   const client = createClient();
   const data = await fetchIntro(client, locale);
   const counts = await fetchCounts(client, locale);
+  const menu = await fetchMenu(client, locale);
 
   return {
     revalidate: 360,
     props: {
       ...translations,
       ...data,
-      counts
+      counts,
+      menu
     }
   };
 }
